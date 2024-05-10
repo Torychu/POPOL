d790574b0849398a6cea2d5477e38245feed7658
정윤환 <torychu0315@naver.com>
2024-05-10 11:00:46 +0900

    dsds

--- frontPopol/app/(root)/_components/navbar.tsx
+++ frontPopol/app/(root)/_components/navbar.tsx
@@ -1,5 +1,16 @@
+import styles from "../../styles/navbar.module.scss";
+
 const Navbar = () => {
-  return <div>hello nav</div>;
+	return (
+		<nav className={styles.navbarBackground}>
+			<div>
+				<div>
+					<div className={styles.navbarMainFont}>PORTFOLIO</div>
+					<div className={styles.navbarMiniFont}>정윤환</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
